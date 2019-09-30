cde914ad716a151483e2600b56be9292ac7028aa
George Rong <37315111+ronggeorge@users.noreply.github.com>
2019-09-30 15:37:31 +0930

    Connection to Database and Login
    Very quick and dirty login page utilising mongodb atlas, addresses #2

--- app.py
+++ app.py
@@ -1,10 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, session, redirect
+from flask_pymongo import PyMongo
+import bcrypt
+
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'Login'
+app.config['MONGO_URI'] = 'mongodb+srv://ssegroup2:MghlwoCbDqaav9Yc@ssegroup2-vksod.mongodb.net/users?retryWrites=true&w=majority'
 
+mongo = PyMongo(app)
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if (request.form['pass'], login_user['password'] == login_user['password']):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.secret_key = 'mysecret'
+    app.run(debug=True)
