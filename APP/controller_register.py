7fa21c3da26b794f7c031a30ffdd26f23bffeed8
srin91297 <ssakibanda@gmail.com>
2019-10-08 10:54:06 +1030

    Made login more pretty

--- APP/controller_register.py
+++ APP/controller_register.py
@@ -1,6 +1,6 @@
 # imports
 from app import app, mongo
-from flask import Flask, render_template, url_for, request, session, redirect
+from flask import Flask, flash, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
 import bcrypt
 
@@ -10,13 +10,21 @@ def register():
     if request.method == 'POST':
         users = mongo.db.users
         existing_user = users.find_one({'name' : request.form['username']})
-
+        
+        #checking if user exists in db
         if existing_user is None:
+            #add user
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
             users.insert({'name' : request.form['username'], 'password' : hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('register'))
-        
-        return 'That username already exists!'
+            # session['username'] = request.form['username']
+            # flash('user successfully created')
+            return render_template('login.html', mess='user successfully created')
 
-    return render_template('register.html')
+        #already exists
+        flash('That user already exists!')
+        return render_template('register.html')   
+
+    if request.method == "GET":
+        return render_template('register.html')
+
+    return '404'
