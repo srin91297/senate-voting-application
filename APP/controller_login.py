399b72cc7654ddefd14d3b4c9c1bc6b3ddeddbd3
srin91297 <ssakibanda@gmail.com>
2019-10-08 12:03:45 +1030

    #7-ssv completed

--- APP/controller_login.py
+++ APP/controller_login.py
@@ -3,19 +3,32 @@ from app import app, mongo
 from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
 import bcrypt
+import cgi
 
 @app.route('/login', methods=['POST', 'GET'])
 
 def login():
     if request.method == "POST":
         users = mongo.db.users
+
+        #server side validaton
+        transform_username = cgi.escape(request.form['username'])
+        transform_password = cgi.escape(request.form['pass'])
+        if transform_username  != request.form['username'] or transform_password  != request.form['pass']:
+            #wrong characters entered
+            return render_template('login.html', mess='Malicious characters entered')
+        
+        #check fields entered are not empty
+        if transform_password == "" or transform_username == "":
+            return render_template('login.html', mess='Must fill in all fields!')  
+
         login_user = users.find_one({'name' : request.form['username']})
 
         if login_user:
             if (request.form['pass'], login_user['password'] == login_user['password']):
                 session['username'] = request.form['username']
                 return redirect(url_for('index'))
-                
+
         #show message of incorrect details
         return render_template('login.html', mess='Incorrect login details entered')
     
--- APP/controller_register.py
+++ APP/controller_register.py
@@ -3,14 +3,29 @@ from app import app, mongo
 from flask import Flask, flash, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
 import bcrypt
+import cgi
 
 @app.route('/register', methods=['POST', 'GET'])
 
 def register():
     if request.method == 'POST':
         users = mongo.db.users
+
+        #server side validaton
+        transform_username = cgi.escape(request.form['username'])
+        transform_password = cgi.escape(request.form['pass'])
+        if transform_username  != request.form['username'] or transform_password  != request.form['pass']:
+            #wrong characters entered
+            flash('Malicious characters entered')
+            return render_template('register.html')  
+
         existing_user = users.find_one({'name' : request.form['username']})
         
+        #check fields entered are not empty
+        if transform_password == "" or transform_username == "":
+            flash('Must fill in all fields!')
+            return render_template('register.html')            
+
         #checking if user exists in db
         if existing_user is None:
             #add user
