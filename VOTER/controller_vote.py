e05897b482bda40f5a50b4c27c6b6a21c4d3355b
George Rong <37315111+ronggeorge@users.noreply.github.com>
2019-11-01 16:31:23 +1030

    voting page above line

--- VOTER/controller_vote.py
+++ VOTER/controller_vote.py
@@ -1,17 +1,33 @@
 # imports
-from app import app, mongo
+from app import app, mongo, common
 from flask import Flask, flash, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
 import bcrypt
 import cgi
 
+#def sorter(e):
+    #return e['name']
+
 @app.route('/vote', methods=['POST', 'GET'])
 
 def vote():
+    parties=common.db.party.find()
     if request.method == 'GET':
-        return render_template('vote.html')
+        return render_template('vote.html', parties=parties)
     if request.method == 'POST':
-        votes = mongo2.db.votes
+        votes = common.db.votes
+        count=parties.count()
+        value=[None]*count
+        for i in parties:
+            if(request.form[i['name']] != ''):
+                x=({i['name']: request.form[i['name']]})
+                value[int(request.form[i['name']])-1] = i['name']
+        
+        for j in range(0,5):
+            if (value[j] == None):
+                return render_template('voterdashboard.html')
+        votes.insert({'vote': value})
+        return render_template('voterdashboard.html')
 
 
     return '404'
